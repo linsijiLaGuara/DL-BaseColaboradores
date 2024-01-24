608f8d84b3e47757aadf4eed705fe1e098c423a4
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-01-24 16:56:07 -0300

    nueva convinacion para los colorte

--- baseDatosColaboradores/src/components/Alert.jsx
+++ baseDatosColaboradores/src/components/Alert.jsx
@@ -1,23 +1,18 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import Alert from "react-bootstrap/Alert";
 
 const Alerta = ({ mensaje, color }) => {
-  <>
-    return(
-    {mensaje && ([
-      'success',
-      'danger'
-    ].map((color) => (
-      <Alert variant={color}>
-        {mensaje}
-      </Alert>
-    )
+  const textColorClass = color === "success" ? "text-success" : "text-danger";
 
-
-    ))});
-
-  </>
+  return (
+    <>
+      {mensaje && (
+        <Alert variant={color} className={textColorClass}>
+          {mensaje}
+        </Alert>
+      )}
+    </>
+  );
 };
 
 export default Alerta;
