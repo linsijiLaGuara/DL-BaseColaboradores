e46d227b90be2aedc2b8b207bd0d8187a40fc6ef
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-01-24 16:51:23 -0300

    tratando de que las letras tengan color

--- baseDatosColaboradores/src/components/Alert.jsx
+++ baseDatosColaboradores/src/components/Alert.jsx
@@ -3,15 +3,21 @@ import React from "react";
 import Alert from "react-bootstrap/Alert";
 
 const Alerta = ({ mensaje, color }) => {
-  return (
-    <>
-      {mensaje && (
-        <Alert variant={color}>
-          {mensaje}
-        </Alert>
-      )}
-    </>
-  );
+  <>
+    return(
+    {mensaje && ([
+      'success',
+      'danger'
+    ].map((color) => (
+      <Alert variant={color}>
+        {mensaje}
+      </Alert>
+    )
+
+
+    ))});
+
+  </>
 };
 
 export default Alerta;
